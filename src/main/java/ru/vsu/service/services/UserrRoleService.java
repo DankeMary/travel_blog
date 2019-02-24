c3be98a40b287e2907cbc4d3993a43f2595e1c7a
DankeMary <laktionofs@mail.ru>
2019-02-24 17:24:12 +0300

    Service classes created

---
+++ src/main/java/ru/vsu/service/services/UserrRoleService.java
@@ -0,0 +1,9 @@
+package ru.vsu.service.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service("userrRoleService")
+@Transactional
+public class UserrRoleService {
+}
